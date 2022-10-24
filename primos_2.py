d9984446e9e84a9c680cf6fd63d3f5ec42fa269a
unknown <sebastian.sarmiento@insoftar.com>
2022-10-24 18:06:32 -0500

    Ejercico fizz-buzz

--- primos_2.py
+++ primos_2.py
@@ -2,7 +2,7 @@
 
 numero = int(input("Ingrese un numero: "))
 
-contador = 0
+contador_de_ceros = 0
 i = 2
 
 if(numero > 1):
@@ -10,10 +10,10 @@ if(numero > 1):
     while(i < numero):
         resto = numero % i
         if(resto == 0):
-            contador = contador + 1
+            contador_de_ceros = contador_de_ceros + 1
         i = i + 1
     
-    if(contador == 0):
+    if(contador_de_ceros == 0):
         print(f"El numero {numero} es primo")
     else:
         print(f"El numero {numero} no es primo")
